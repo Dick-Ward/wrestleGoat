45a8eee89d3132c4d966757d1d14a89e034fa1cc
gonzodamus <github email address>
2018-05-16 14:58:06 -0400

    wrestler data includes goat attribute

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import wrestlers from "./data/wrestlers"
+import WrestlerList from "./containers/wrestlerList"
 
 class App extends Component {
   render() {
     console.log(wrestlers)
     return (
       <div>
-      hi
+      <WrestlerList />
       </div>
     );
   }
--- src/data/wrestlers.js
+++ src/data/wrestlers.js
@@ -1,16 +1,20 @@
 const wrestlers = [
   {name: "Hulk Hogan",
    pro: "Charisma",
-   con: "Terrible Worker"},
+   con: "Terrible Worker",
+   goat: false},
   {name: "John Cena",
    pro: "Not Hogan",
-   con: "Some Fans Really Hate Him"},
+   con: "Some Fans Really Hate Him",
+   goat: false},
   {name: "Stone Cold Steve Austin",
    pro: "Amazing Brawler and Beer Drinker",
-   con: "Unsuccessful Heel Turn"},
+   con: "Unsuccessful Heel Turn",
+   goat: false},
   {name: "Shawn Michaels",
    pro: "Sells Like a MF",
-   con: "Lost his Smile"}
+   con: "Lost his Smile",
+   goat: false}
 ]
 
 export default wrestlers
